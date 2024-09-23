0b5921d2b4cd36339b7e61aae746577a1feb78c9
Tundra <78630930+Tundra8946@users.noreply.github.com>
2024-09-22 22:32:58 -0400

    feat: Updating the status of Acro and fixing the disabled tag

--- src/app/(default)/projects/page.js
+++ src/app/(default)/projects/page.js
@@ -24,6 +24,8 @@ export default function ProjectsPage() {
                 return "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300";
             case "Pending":
                 return "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300";
+            case "Disabled":
+                return "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300";
             default:
                 return "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300";
         }
